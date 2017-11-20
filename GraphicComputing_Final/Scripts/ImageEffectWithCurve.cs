2f5ef46c94170389df08304216b1bdbced34b764
xoakzxz <juandavid_ja@hotmail.com>
2017-11-19 22:36:25 -0500

    git add .

---
+++ GraphicComputing_Final/Scripts/ImageEffectWithCurve.cs
@@ -0,0 +1,162 @@
+﻿using UnityEngine;
+
+[ExecuteInEditMode]
+public class ImageEffectWithCurve : MonoBehaviour {
+
+    #region Properties
+
+    public Shader shader;
+    public float texelSize = 1f;
+
+    [Space(10f)]
+
+    [SerializeField]
+    private AnimationCurve curve;
+    [SerializeField]
+    private int kernelSize; //For 3x3 kernel => kernelSize = 3; For 5x5 kernel => kernelSize = 5; . . .
+    [SerializeField]
+    private float factor;
+
+    //Kernel
+    private float[,] kernel;
+    private float step; //For 3x3 kernel => step = 0.1666666667 (1/6); For 5x5 kernel => step = 0.1 (1/10); . . .
+    private float normalize;
+
+    //Hidden
+    private float lastTexelSize;
+    private float lastCurve;
+    private float lastFactor;
+
+    private Material mainMaterial;
+    private Material MainMaterial
+    {
+        get
+        {
+            if (mainMaterial != null)
+            {
+                return mainMaterial;
+            }
+            else
+            {
+                mainMaterial = new Material(shader);
+                mainMaterial.hideFlags = HideFlags.HideAndDontSave;
+            }
+
+            return mainMaterial;
+        }
+    }
+
+    #endregion
+
+    #region Unity Functions
+
+    private void Start()
+    {
+        if (!SystemInfo.supportsImageEffects)
+        {
+            enabled = false;
+            return;
+        }
+
+        if(!shader || !shader.isSupported)
+        {
+            enabled = false;
+            return;
+        }
+
+        SetupKernel();
+    }
+
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        if (lastTexelSize != texelSize)
+            MainMaterial.SetFloat("_TexelSize", texelSize);
+
+        if (lastCurve != curve.Evaluate(0.33f) + curve.Evaluate(0.66f) + curve.Evaluate(0.99f))
+            EstimateKernel();
+
+        if (lastFactor != factor)
+            MainMaterial.SetFloat("_Factor", factor);
+
+        Graphics.Blit(source, destination, MainMaterial);
+
+        lastTexelSize = texelSize;
+
+        lastCurve = curve.Evaluate(0.33f) + curve.Evaluate(0.66f) + curve.Evaluate(0.99f);
+
+        lastFactor = factor;
+    }
+
+    private void OnEnable()
+    {
+        SetupKernel();
+    }
+
+    private void OnDisable()
+    {
+        if (mainMaterial != null)
+            DestroyImmediate(mainMaterial);
+    }
+
+    #endregion
+
+    #region Class Functions
+
+    private void SetupKernel()
+    {
+        kernel = new float[kernelSize, kernelSize];
+
+        step = (1.0f / (kernelSize * 2.0f));
+
+        normalize = (1.0f / kernel.Length);
+
+        EstimateKernel();
+    }
+
+    private void EstimateKernel()
+    {
+        float rowEvaluatePoint = 1 - step;
+        float columnEvaluatePoint = step;
+
+        float rowEvaluateValue;
+        float columnEvaluateValue;
+
+        for (int row = 0; row < kernelSize; row++)
+        {
+            rowEvaluateValue = curve.Evaluate(rowEvaluatePoint);
+
+            for (int column = 0; column < kernelSize; column++)
+            {
+                columnEvaluateValue = curve.Evaluate(columnEvaluatePoint);
+
+                float kernelFinalValue = 
+                    (rowEvaluateValue + 
+                    columnEvaluateValue) * normalize;
+
+                kernel[row, column] = kernelFinalValue;
+
+                /*
+                Debug.Log(string.Format("{0},{1} = {2}",
+                    row.ToString(),
+                    column.ToString(),
+                    kernelFinalValue));
+                */
+
+                MainMaterial.SetFloat(string.Format("_m{0}{1}",
+                    row.ToString(),
+                    column.ToString()),
+                    kernelFinalValue);
+
+                columnEvaluatePoint += (2 * step);
+            }
+
+            columnEvaluatePoint = step;
+
+            rowEvaluatePoint -= (2 * step);
+        }
+
+        rowEvaluatePoint = 1 - step;
+    }
+
+    #endregion
+}
---
+++ GraphicComputing_Final/Shaders/CurveShader3x3.shader
@@ -0,0 +1,100 @@
+﻿Shader "Hidden/CurveShader3x3"
+{
+	Properties
+	{
+		_MainTex("Texture", 2D) = "white" {}
+		_TexelSize("Texel Size", Float) = 1
+		_Factor("Factor", Float) = 1
+		//Matrix values
+		_m00("m00", Float) = 0
+		_m01("m01", Float) = 0
+		_m02("m02", Float) = 0
+		_m10("m10", Float) = 0
+		_m11("m11", Float) = 0
+		_m12("m12", Float) = 0
+		_m20("m20", Float) = 0
+		_m21("m21", Float) = 0
+		_m22("m22", Float) = 0
+	}
+
+	SubShader
+	{
+		Cull Off ZWrite Off ZTest Always
+
+		Pass
+		{
+			CGPROGRAM
+
+			#pragma vertex vert_img
+			#pragma fragment frag
+			#pragma fragmentoption ARB_precision_hint_fastest
+
+			#include "UnityCG.cginc"
+
+			//PROPERTIES!
+			sampler2D _MainTex;
+			float4 _MainTex_TexelSize;
+			float _TexelSize;
+			float _Factor;
+			
+			//KERNEL!
+			float _m00;
+			float _m01;
+			float _m02;
+			float _m10;
+			float _m11;
+			float _m12;
+			float _m20;
+			float _m21;
+			float _m22;
+
+			//CONVULTION!
+			inline float3 Convultion(float2 uv, float2 texelSize, float3x3 kernel)
+			{
+				float3 finalColor;
+
+				for (int row = 0; row < 3; row++)
+				{
+					for (int column = 0; column < 3; column++)
+					{
+						//new UV
+						float2 newUV = float2(
+							(uv.x - texelSize.x) + column * texelSize.x,
+							(uv.y - texelSize.y) + row * texelSize.y
+						);
+
+						//New color
+						float3 newColor = tex2D(_MainTex, newUV);
+
+						//Returned color
+						finalColor += newColor * kernel[row][column];
+					}
+				}
+
+				return finalColor;
+			}
+
+			//FRAG FUNCTION!
+			float4 frag(v2f_img i) : SV_Target
+			{
+				float2 texelSize = _MainTex_TexelSize * _TexelSize;
+
+				//Kernel declaration
+				float3x3 kernel = float3x3(
+					_m00 * _Factor, _m01 * _Factor, _m02 * _Factor, //First row
+					_m10 * _Factor, _m11 * _Factor, _m12 * _Factor, //Second row
+					_m20 * _Factor, _m21 * _Factor, _m22 * _Factor //Third row
+				);
+
+				float4 finalImage;
+
+				finalImage.rgb = Convultion(i.uv, texelSize, kernel);
+
+				return finalImage;
+			}
+
+			ENDCG
+		}
+	}
+}
+
